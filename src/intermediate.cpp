c4d9d8a70c36f8565097276dec3599432e3aedae
Dominik Penk <dominik.penk@fau.de>
2020-11-18 07:37:06 +0100

    Updated intermediate texture drawing

--- src/intermediate.cpp
+++ src/intermediate.cpp
@@ -1,29 +1,17 @@
 #include "intermediate.h"
 
-const std::string vertex_display_shader_up = R"(
+const std::string vertex_display_shader = R"(
 #version 430
 out vec2 texCoord;
+
+uniform vec2 uvmin;
+uniform vec2 uvmax;
 
 void main()
 {
     float x = -1.0 + float((gl_VertexID & 1) << 2);
     float y = -1.0 + float((gl_VertexID & 2) << 1);
-    texCoord.x = (x+1.0)*0.5;
-    texCoord.y = (y+1.0)*0.5;
-	gl_Position = vec4(x, y, 0, 1);
-}
-)";
-
-const std::string vertex_display_shader_down = R"(
-#version 430
-out vec2 texCoord;
-
-void main()
-{
-    float x = -1.0 + float((gl_VertexID & 1) << 2);
-    float y = -1.0 + float((gl_VertexID & 2) << 1);
-    texCoord.x = (x+1.0)*0.5;
-    texCoord.y = 1.0 - (y+1.0)*0.5;
+    texCoord = mix(uvmin, uvmax, vec2((x+1.0)*0.5, (y+1.0)*0.5));
 	gl_Position = vec4(x, y, 0, 1);
 }
 )";
@@ -42,39 +30,32 @@ void main()
 }
 )";
 
-std::shared_ptr<gl::Shader> gl::textureDispShader(bool uvUp)
+std::shared_ptr<gl::Shader> gl::textureDispShader()
 {
-    static std::shared_ptr<gl::Shader> up_shader = nullptr;
-    static std::shared_ptr<gl::Shader> down_shader = nullptr;
+    static std::shared_ptr<gl::Shader> shader = nullptr;
 
-    if (uvUp) {
-        if (up_shader == nullptr) {
-            up_shader = std::make_shared<gl::Shader>(std::initializer_list<std::pair<GLenum, std::string>>{
-                { GL_VERTEX_SHADER, vertex_display_shader_up },
-                { GL_FRAGMENT_SHADER, fragment_display_shader }
-            });
-        }
-        return up_shader;
+    if (shader == nullptr) {
+        shader = std::make_shared<gl::Shader>(std::initializer_list<std::pair<GLenum, std::string>>{
+            { GL_VERTEX_SHADER, vertex_display_shader },
+            { GL_FRAGMENT_SHADER, fragment_display_shader }
+        });
     }
-    else {
-        if (down_shader == nullptr) {
-            down_shader = std::make_shared<gl::Shader>(std::initializer_list<std::pair<GLenum, std::string>>{
-                { GL_VERTEX_SHADER, vertex_display_shader_down },
-                { GL_FRAGMENT_SHADER, fragment_display_shader }
-            });
-        }
-        return down_shader;
-    }
+    return shader;
 }
 
-void gl::displayTexture(int x, int y, int width, int height, std::shared_ptr<gl::Texture> tex, bool uvUp)
+void gl::displayTexture(int x, int y, int width, int height, std::shared_ptr<gl::Texture> tex, glm::vec2 uvmin, glm::vec2 uvmax)
 {
-    fullscreenTriangle(x, y, width, height, gl::textureDispShader(uvUp),
-        "image", tex);
+    fullscreenTriangle(x, y, width, height, gl::textureDispShader(),
+        "image", tex,
+        "uvmin", uvmin,
+        "uvmax", uvmax);
 }
 
-void gl::displayTexture(int x, int y, int width, int height, gl::Texture& tex, bool uvUp)
+void gl::displayTexture(int x, int y, int width, int height, gl::Texture& tex, glm::vec2 uvmin, glm::vec2 uvmax)
 {
-    fullscreenTriangle(x, y, width, height, gl::textureDispShader(uvUp),
-        "image", tex);
+    fullscreenTriangle(x, y, width, height, gl::textureDispShader(),
+        "image", tex,
+        "uvmin", uvmin,
+        "uvmax", uvmax);
 }
+
--- src/intermediate.h
+++ src/intermediate.h
@@ -4,7 +4,7 @@
 
 namespace gl {
 
-	std::shared_ptr<gl::Shader> textureDispShader(bool uvUp = true);
+	std::shared_ptr<gl::Shader> textureDispShader();
 
 	template<typename... Uniforms>
 	void fullscreenTriangle(int x, int y, int width, int height, gl::Shader& shader, Uniforms... uniforms);
@@ -12,8 +12,9 @@ namespace gl {
 	template<typename... Uniforms>
 	void fullscreenTriangle(int x, int y, int width, int height, std::shared_ptr<gl::Shader> shader, Uniforms... uniforms);
 
-	void displayTexture(int x, int y, int width, int height, std::shared_ptr<gl::Texture> tex, bool uvUp = true);
-	void displayTexture(int x, int y, int width, int height, gl::Texture& tex, bool uvUp = true);
+
+	void displayTexture(int x, int y, int width, int height, std::shared_ptr<gl::Texture> tex, glm::vec2 uvmin = glm::vec2(0), glm::vec2 umax = glm::vec2(1));
+	void displayTexture(int x, int y, int width, int height, gl::Texture& tex, glm::vec2 uvmin = glm::vec2(0), glm::vec2 uvmax = glm::vec2(1));
 
 }
 
