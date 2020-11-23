97d4f6586b4177d0533258e662fef020f0beed17
Dominik Penk <dominik.penk@fau.de>
2020-11-23 12:34:18 +0100

    Added some more integral input fields to imgui_ext.hpp

--- include/glpp/imgui_ext.hpp
+++ include/glpp/imgui_ext.hpp
@@ -10,4 +10,8 @@ namespace ImGui {
 
 	void Image(std::shared_ptr<gl::Texture> tex, const ImVec2& size, const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1), const ImVec4& tint_col = ImVec4(1, 1, 1, 1), const ImVec4& border_col = ImVec4(0, 0, 0, 0));
 	bool ImageButton(std::shared_ptr<gl::Texture> tex, const ImVec2& size, const ImVec2& uv0 = ImVec2(0, 0), const ImVec2& uv1 = ImVec2(1, 1), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0, 0, 0, 0), const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
+
+	bool InputUInt(const char* label, uint32_t* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
+	bool InputInt64(const char* label, int64_t* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
+	bool InputUInt64(const char* label, uint64_t* v, int step = 1, int step_fast = 100, ImGuiInputTextFlags flags = 0);
 }
--- src/imgui_ext.cpp
+++ src/imgui_ext.cpp
@@ -11,3 +11,22 @@ bool ImGui::ImageButton(std::shared_ptr<gl::Texture> tex, const ImVec2& size, co
 {
 	return ImGui::ImageButton(*tex, size, uv0, uv1, frame_padding, bg_col, tint_col);
 }
+
+bool ImGui::InputUInt(const char* label, uint32_t* v, int step, int step_fast, ImGuiInputTextFlags flags)
+{
+	const char* format = (flags & ImGuiInputTextFlags_CharsHexadecimal) ? "%08X" : "%u";
+	return InputScalar(label, ImGuiDataType_U32, (void*)v, (void*)(step > 0 ? &step : NULL), (void*)(step_fast > 0 ? &step_fast : NULL), format, flags);
+
+}
+
+bool ImGui::InputInt64(const char* label, int64_t* v, int step, int step_fast, ImGuiInputTextFlags flags)
+{
+	const char* format = (flags & ImGuiInputTextFlags_CharsHexadecimal) ? "%08X" : "%lld";
+	return InputScalar(label, ImGuiDataType_S64, (void*)v, (void*)(step > 0 ? &step : NULL), (void*)(step_fast > 0 ? &step_fast : NULL), format, flags);
+}
+
+bool ImGui::InputUInt64(const char* label, uint64_t* v, int step, int step_fast, ImGuiInputTextFlags flags)
+{
+	const char* format = (flags & ImGuiInputTextFlags_CharsHexadecimal) ? "%08X" : "%llu";
+	return InputScalar(label, ImGuiDataType_U64, (void*)v, (void*)(step > 0 ? &step : NULL), (void*)(step_fast > 0 ? &step_fast : NULL), format, flags);
+}
