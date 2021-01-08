fe59b036c6501a2dc736c07463f7ea5c45d6218e
Dominik Penk <penk.dominik@gmail.com>
2021-01-08 09:19:34 +0100

    Updated imgui and implemented vertical tabs with tables api

--- include/glpp/imgui_vertical_tabs.hpp
+++ include/glpp/imgui_vertical_tabs.hpp
@@ -2,25 +2,38 @@
 
 #include "glpp/imgui.hpp"
 
+typedef int ImGuiVerticalTabFlags;
+
+enum ImGuiVerticalTabFlags_ {
+	ImGuiVerticalTabFlags_None  = 0,
+	ImGuiVerticalTabFlags_Right = 1 << 1,	// Tabs on the right side
+};
+
 namespace ImGui {
+
 
 	struct ImVerticalTabBar;
 
 	struct ImVerticalTabBar {
 		ImVerticalTabBar() :
 			activeTabID(0),
-			currentTabID(0),
-			tabChildID(0) {}
+			cursorPos(0, 0),
+			tabColumn(0) {}
 
-		static ImVerticalTabBar*        CurrentVerticalTabBar;
-		static ImPool<ImVerticalTabBar> VerticalTabBars;
+		ImVector<const char*> labels;
+
+		static ImPool<ImVerticalTabBar>    VerticalTabBars;
+		static ImVector<ImVerticalTabBar*> VerticalTabBarStack;
+
+		static ImVerticalTabBar* CurrentVerticalTabBar() { return VerticalTabBarStack.empty() ? NULL : VerticalTabBarStack[VerticalTabBarStack.Size - 1]; }
 		
 		ImGuiID activeTabID;
-		ImGuiID currentTabID;
-		ImGuiID tabChildID;
+		ImVec2  cursorPos;
+		int     tabColumn;
 	};
 
-	bool BeginVerticalTabBar(const char* id);
+	bool BeginVerticalTabBar(ImGuiID id, ImGuiVerticalTabFlags flags = 0);
+	bool BeginVerticalTabBar(const char* str_id, ImGuiVerticalTabFlags flags = 0);
 	void EndVerticalTabBar();
 
 	bool BeginVerticalTabItem(const char* label);
--- src/imgui_vertical_tabs.cpp
+++ src/imgui_vertical_tabs.cpp
@@ -3,82 +3,68 @@
 #include <iostream>
 
 ImPool<ImGui::ImVerticalTabBar> ImGui::ImVerticalTabBar::VerticalTabBars;
-ImGui::ImVerticalTabBar* ImGui::ImVerticalTabBar::CurrentVerticalTabBar = NULL;
+ImVector<ImGui::ImVerticalTabBar*> ImGui::ImVerticalTabBar::VerticalTabBarStack;
 
-bool ImGui::BeginVerticalTabBar(const char* str_id)
+bool ImGui::BeginVerticalTabBar(ImGuiID id, ImGuiVerticalTabFlags flags)
 {
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
     if (window->SkipItems)
         return false;
 
-	// Currently we cannot nest vertical tabs
-    if (ImVerticalTabBar::CurrentVerticalTabBar != NULL)
-    {
-        IM_ASSERT_USER_ERROR(ImVerticalTabBar::CurrentVerticalTabBar == NULL, "Nested vertical tabs are not supported (did you miss to call EndVerticalTabBar()?)");
-        return false;
-    }
-
-    ImGuiID id = window->GetID(str_id);
     ImVerticalTabBar* tabBar = ImVerticalTabBar::VerticalTabBars.GetOrAddByKey(id);
-    ImVerticalTabBar::CurrentVerticalTabBar = tabBar;
+    tabBar->tabColumn = flags & ImGuiVerticalTabFlags_Right ? 1 : 0;
+    ImVerticalTabBar::VerticalTabBarStack.push_back(tabBar);
 
     ImGui::PushID(id);
 
-    // Create the child for rendering later on
-    tabBar->tabChildID = window->GetID("TabBar");
+    const ImGuiTableFlags tableFlags = 0;
+    const bool tableVisible = BeginTable("##VerticalTabLayout", 2, tableFlags, GetContentRegionAvail());
+    if (!tableVisible) { PopID(); }
+    else { 
+        TableSetupColumn("Col0", tabBar->tabColumn == 0 ? ImGuiTableColumnFlags_WidthAuto : ImGuiTableColumnFlags_WidthStretch);
+        TableSetupColumn("Col1", tabBar->tabColumn == 1 ? ImGuiTableColumnFlags_WidthAuto : ImGuiTableColumnFlags_WidthStretch);
+        TableNextRow(); 
+        TableSetColumnIndex(tabBar->tabColumn);
+        tabBar->cursorPos = GetCursorPos();
+    }
+    return tableVisible;
+}
 
-    ImGui::PushStyleColor(ImGuiCol_ChildBg, ImGui::GetStyleColorVec4(ImGuiCol_TitleBg));
-    ImGui::BeginChild(tabBar->tabChildID, ImVec2(60, 0));       // FIXME: make width dynamic (or set it as parameter)
-    ImGui::PopStyleColor(1);
+bool ImGui::BeginVerticalTabBar(const char* str_id, ImGuiVerticalTabFlags flags) {
+    ImGuiContext& g = *GImGui;
+    ImGuiWindow* window = g.CurrentWindow;
+    if (window->SkipItems)
+        return false;
 
-    // Draw Seperator
-    ImVec2 tl = ImGui::GetWindowPos() + ImVec2(59, 0);
-    ImU32 col = ImGui::ColorConvertFloat4ToU32(ImGui::GetStyleColorVec4(ImGuiCol_Separator));
-    ImGui::GetWindowDrawList()->AddLine(tl, tl + ImVec2(0, ImGui::GetWindowHeight()), col);
-    
-    ImGui::EndChild();
+    ImGuiID id = window->GetID(str_id);
+
+    return BeginVerticalTabBar(id, flags);
 }
 
 void ImGui::EndVerticalTabBar()
 {
-    ImGuiContext& g = *GImGui;
-    ImGuiWindow* window = g.CurrentWindow;
-    if (window->SkipItems)
-        return;
-
-    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar;
+    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar();
     if (tabBar == NULL) {
         IM_ASSERT_USER_ERROR(tabBar != NULL, "Mismatched BeginVerticalTabBar()/EndVerticalTabBar()!");
         return;
     }
 
-    if (tabBar->currentTabID != 0) {
-        IM_ASSERT_USER_ERROR(tabBar->currentTabID != NULL, "Missing EndVerticalTabBarItem()!");
-        return;
-    }
-
     // Pop the tab id from the stack
+    ImGui::EndTable();
     ImGui::PopID();
-    ImVerticalTabBar::CurrentVerticalTabBar = NULL;
+    ImVerticalTabBar::VerticalTabBarStack.pop_back();
 }
 
 bool ImGui::BeginVerticalTabItem(const char* label)
 {
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
-    if (window->SkipItems)
-        return false;
 
-    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar;
+    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar();
     if (tabBar == NULL)
     {
         IM_ASSERT_USER_ERROR(tabBar, "Needs to be called between BeginVerticalTabBar() and EndVerticalTabBar()!");
-        return false;
-    }
-
-    if (tabBar->currentTabID != 0) {
-        IM_ASSERT_USER_ERROR(tabBar->currentTabID == 0, "Mismatched BeginVerticalTabItem()/EndVerticalTabItem()!");
         return false;
     }
 
@@ -87,48 +73,31 @@ bool ImGui::BeginVerticalTabItem(const char* label)
         tabBar->activeTabID = thisTabID;
     }
 
-    // Render the button in the button child
-    ImGui::BeginChild(tabBar->tabChildID);
-    // Style like tabs 
-    // FIXME: Make active tab more visible
-    bool active = thisTabID == tabBar->activeTabID;
-    ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetStyleColorVec4(active ? ImGuiCol_TabUnfocusedActive : ImGuiCol_TabUnfocused));
-    ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImGui::GetStyleColorVec4(ImGuiCol_TabActive));
-    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImGui::GetStyleColorVec4(ImGuiCol_TabHovered));
-    if (ImGui::Button(label)) {
-        tabBar->activeTabID = thisTabID;
+    // Render the Button to the left most column
+    const bool active = thisTabID == tabBar->activeTabID;
+    if (TableSetColumnIndex(tabBar->tabColumn)) {
+        // Style like tabs 
+        // FIXME: Make active tab more visible
+        ImGui::PushStyleColor(ImGuiCol_Button, ImGui::GetStyleColorVec4(active ? ImGuiCol_TabUnfocusedActive : ImGuiCol_TabUnfocused));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImGui::GetStyleColorVec4(ImGuiCol_TabActive));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImGui::GetStyleColorVec4(ImGuiCol_TabHovered));
+        SetCursorPos(tabBar->cursorPos);
+        if (ImGui::Button(label)) {
+            tabBar->activeTabID = thisTabID;
+        }
+        tabBar->cursorPos = GetCursorPos();
+        ImGui::PopStyleColor(3);
     }
-    ImGui::PopStyleColor(3);
-    ImGui::EndChild();
-
-    if (thisTabID == tabBar->activeTabID) {
-        ImGui::SameLine();
-        ImGui::BeginChild(thisTabID);
-        tabBar->currentTabID = thisTabID;
-        return true;
-    }
-    return false;
+    return active && TableSetColumnIndex((tabBar->tabColumn + 1) % 2);
 }
 
 void ImGui::EndVerticalTabItem()
 {
     ImGuiContext& g = *GImGui;
-    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar;
+    ImVerticalTabBar* tabBar = ImVerticalTabBar::CurrentVerticalTabBar();
     if (tabBar == NULL)
     {
         IM_ASSERT_USER_ERROR(tabBar, "Needs to be called between BeginVerticalTabBar() and EndVerticalTabBar()!");
         return;
     }
-
-    if (tabBar->currentTabID == 0) {
-        IM_ASSERT_USER_ERROR(tabBar->currentTabID != 0, "Mismatched BeginVerticalTabItem()/EndVerticalTabItem()!");
-        return;
-    }
-
-    if (tabBar->activeTabID != tabBar->currentTabID) {
-        IM_ASSERT_USER_ERROR(tabBar->activeTabID == tabBar->currentTabID, "You should not call EndVerticalTabItem() if the tab is not the active one");
-    }
-
-    ImGui::EndChild();
-    tabBar->currentTabID = 0;
 }
