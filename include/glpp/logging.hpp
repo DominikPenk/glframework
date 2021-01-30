cd0f051e094bf4b0746d213fe5901795e966a42d
Dominik Penk <penk.dominik@gmail.com>
2021-01-30 16:53:10 +0100

    Even more logging.

--- include/glpp/logging.hpp
+++ include/glpp/logging.hpp
@@ -4,23 +4,51 @@
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 #define LOG(msg) gl::Logging::Dispatch(__FILE__, __LINE__, gl::LogLevel_Info, (msg))
 #define LOG(format, ...) gl::Logging::Dispatchf(__FILE__, __LINE__, gl::LogLevel_Info, format, __VA_ARGS__)
 #define LOG_IF(cond, msg) if((cond)) { LOG(msg); }
+#define LOG_IF(cond, format, ...) if((cond)) { LOG(format, __VA_ARGS__); }
+#define LOG_ONCE(msg) { static bool __first_call__ = true; LOG_IF(__first_call__, msg); __first_call__ = false; }
+#define LOG_ONCE(format, ...) { static bool __first_call__ = true; LOG_IF(__first_call__, format, __VA_ARGS__); __first_call__ = false; }
+#define LOG_EVERY_N(n, msg) { static ptrdiff_t __callcount__ = 0; LOG_IF(__callcount__ % (n) == 0, msg);  __callcount__++; }
+#define LOG_EVERY_N(n, format, ...) { static ptrdiff_t __callcount__ = 0; LOG_IF(__callcount__ % (n) == 0, format, __VA_ARGS__);  __callcount__++; }
+#if !defined(_DEBUG)
+#define DEBUG_LOG(msg)
+#define DEBUG_LOG_IF(cond, msg)
+#else
+#define DEBUG_LOG(msg) LOG(msg)
+#define DEBUG_LOG_IF(cond, msg) LOG_IF(cond, msg)
+#endif
 
 #define LOG_SUCCESS(msg) gl::Logging::Dispatch(__FILE__, __LINE__, gl::LogLevel_Success, (msg))
 #define LOG_SUCCESS(format, ...) gl::Logging::Dispatchf(__FILE__, __LINE__, gl::LogLevel_Success, format, __VA_ARGS__)
 #define LOG_SUCCESS_IF(msg) if((cond)) { LOG_SUCCESS(msg); }
+#define LOG_SUCCESS_IF(format, ...) if((cond)) { LOG_SUCCESS(format, __VA_ARGS__); }
+#define LOG_SUCCESS_ONCE(msg) { static bool __first_call__ = true; LOG_SUCCESS_IF(__first_call__, msg); __first_call__ = false; }
+#define LOG_SUCCESS_ONCE(format, ...) { static bool __first_call__ = true; LOG_SUCCESS_IF(__first_call__, format, __VA_ARGS__); __first_call__ = false; }
+#define LOG_SUCCESS_EVERY_N(n, msg) { static ptrdiff_t __callcount__ = 0; LOG_SUCCESS_IF(__callcount__ % (n) == 0, msg);  __callcount__++; }
+#define LOG_SUCCESS_EVERY_N(n, format, ...) { static ptrdiff_t __callcount__ = 0; LOG_SUCCESS_IF(__callcount__ % (n) == 0, format, __VA_ARGS__);  __callcount__++; }
 
 #define LOG_WARNING(msg) gl::Logging::Dispatch(__FILE__, __LINE__, gl::LogLevel_Warning, (msg))
-#define LOG_Warning(format, ...) gl::Logging::Dispatchf(__FILE__, __LINE__, gl::LogLevel_Warning, format, __VA_ARGS__)
+#define LOG_WARNING(format, ...) gl::Logging::Dispatchf(__FILE__, __LINE__, gl::LogLevel_Warning, format, __VA_ARGS__)
 #define LOG_WARNING_IF(msg) if((cond)) { LOG_WARNING(msg); }
+#define LOG_WARNING_IF(format, ...) if((cond)) { LOG_WARNING(format, __VA_ARGS__); }
+#define LOG_WARNING_ONCE(msg) { static bool __first_call__ = true; LOG_WARNING_IF(__first_call__, msg); __first_call__ = false; }
+#define LOG_WARNING_ONCE(format, ...) { static bool __first_call__ = true; LOG_WARNING_IF(__first_call__, format, __VA_ARGS__); __first_call__ = false; }
+#define LOG_WARNING_EVERY_N(n, msg) { static ptrdiff_t __callcount__ = 0; LOG_WARNING_IF(__callcount__ % (n) == 0, msg);  __callcount__++; }
+#define LOG_WARNING_EVERY_N(n, format, ...) { static ptrdiff_t __callcount__ = 0; LOG_WARNING_IF(__callcount__ % (n) == 0, format, __VA_ARGS__);  __callcount__++; }
 
 #define LOG_ERROR(msg) gl::Logging::Dispatch(__FILE__, __LINE__, gl::LogLevel_Error, (msg))
 #define LOG_EROR(format, ...) gl::Logging::Dispatchf(__FILE__, __LINE__, gl::LogLevel_Error, format, __VA_ARGS__)
 #define LOG_ERROR_IF(msg) if((cond)) { LOG_ERROR(msg); }
+#define LOG_ERROR_IF(format, ...) if((cond)) { LOG_ERROR(format, __VA_ARGS__); }
+#define LOG_ERROR_ONCE(msg) { static bool __first_call__ = true; LOG_ERROR_IF(__first_call__, msg); __first_call__ = false; }
+#define LOG_ERROR_ONCE(format, ...) { static bool __first_call__ = true; LOG_ERROR_IF(__first_call__, format, __VA_ARGS__); __first_call__ = false; }
+#define LOG_ERROR_EVERY_N(n, msg) { static ptrdiff_t __callcount__ = 0; LOG_ERROR_IF(__callcount__ % (n) == 0, msg);  __callcount__++; }
+#define LOG_ERROR_EVERY_N(n, format, ...) { static ptrdiff_t __callcount__ = 0; LOG_ERROR_IF(__callcount__ % (n) == 0, format, __VA_ARGS__);  __callcount__++; }
 
 #define LOG_SUCCESS_OR_WARNING(cond, msg_success, msg_fail) if((cond)) { LOG_SUCCESS(msg_success); } else { LOG_WARNING(msg_fail); }
 #define LOG_SUCCESS_OR_ERROR(cond, msg_success, msg_fail) if((cond)) { LOG_SUCCESS(msg_success); } else { LOG_ERROR(msg_fail); }
@@ -61,9 +89,11 @@ namespace gl {
 			msg(msg) {}
 	};
 	
+
 	struct LoggingEndpoint {
 		virtual void onMessage(const LogMessage& msg) = 0;
 	};
+
 
 	struct Logging {
 		static void Dispatch(std::string file, int line, LogLevel_ level, std::string msg);
@@ -73,6 +103,7 @@ namespace gl {
 			std::snprintf(tmp_buffer, sizeof(tmp_buffer), format, args...);
 			Dispatch(file, line, level, std::string(tmp_buffer));
 		}
+		
 		static void RegisterEndpoint(LoggingEndpoint* endpoint);
 		static void RemoveEndpoint(LoggingEndpoint* endpoint);
 
--- samples/main.cpp
+++ samples/main.cpp
@@ -13,6 +13,10 @@ int main(int argc, const char* argv[]) {
 	auto teapot = editor.addObject<gl::TriangleMesh>("Teapot", std::string(SAMPLE_DIR) + "teapot.obj");
 	LOG_SUCCESS_OR_ERROR(teapot != nullptr, "Loaded teapot mesh", "Could not load teapot mesh");
 	LOG_WARNING("Just a sample warning, nothing to fear!");
+	for (int i = 0; i < 4; ++i) {
+		LOG_ONCE("This is only logged %s!", "once");
+		LOG_EVERY_N(2, "This is logged only if #calls.mod(%d) == 0.", 2);
+	}
 	editor.run();
 
 	return EXIT_SUCCESS;
